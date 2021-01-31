61ecdffc5aed663a4903527e7b2a7a2221bc79cb
Ulrich Bosbach <ulrich.bosbach@eskumed.de>
2021-01-31 13:19:46 +0100

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
